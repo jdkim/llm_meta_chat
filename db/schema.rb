87263ebf1c0be44f9a44d8a589b4e3ec17251a33
dhq_boiler <dhq_boiler@live.jp>
2026-03-19 15:31:12 +0900

    feat: update messages migration with parent reference support

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_27_070100) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_19_052219) do
   create_table "chats", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "llm_uuid"
